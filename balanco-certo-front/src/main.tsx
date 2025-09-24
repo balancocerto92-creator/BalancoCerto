f93cc6af723dd301576fc5cef45d20c4f0be0224
Antonio Carlos <balancocerto92@gmail.com>
2025-09-24 09:16:07 -0300

    Fix: Arquivo Main.tsx 2.0

--- balanco-certo-front/src/main.tsx
+++ balanco-certo-front/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx - VERSÃO CORRIGIDA E FINAL
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// Importa o nosso provedor de autenticação
+import { AuthProvider } from './contexts/AuthContext.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* "Abraçamos" toda a aplicação com o AuthProvider */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+);
